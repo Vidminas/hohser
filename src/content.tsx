6d58de0d1b216b1ac00033b83ea37c60e0cb7612
Vidminas Mikucionis <5411598+Vidminas@users.noreply.github.com>
2024-10-27 14:22:55 +0000

    Fix result tags placement

--- src/content.tsx
+++ src/content.tsx
@@ -230,45 +230,16 @@ function processResult (r: Element, domainList: any, options: any, processResult
 
     if (!fetchedMap[url]) {
       fetchedMap[url] = true;
-      chrome.runtime.sendMessage({type: "searchResult", url: url}, function(text) {
-          const parser = new DOMParser();
-          const doc = parser.parseFromString(text, 'text/html');
-          const article = doc.querySelector('p');
-          // `document.querySelector` may return null if the selector doesn't match anything.
-          if (article) {
-            const articleText = article.textContent;
-            const wordMatchRegExp = /[^\s]+/g; // Regular expression
-            const words = articleText.matchAll(wordMatchRegExp);
-            // matchAll returns an iterator, convert to array to get word count
-            const wordCount = [...words].length;
-            const readingTime = Math.round(wordCount / 200);
-
-            
-            // const badge = document.createElement("p");
-            // Use the same styling as the publish information in an article's header
-            // badge.classList.add("color-secondary-text", "type--caption");
-            // badge.textContent = `⏱️ ${readingTime} min read`;
-          
-
-            // Support for API reference docs
-            // const heading = article.querySelector("h1");
-            // Support for article docs with date
-            // const date = article.querySelector("time")?.parentNode;
-
-            // result.insertAdjacentElement("afterend", badge);
-          } else {
-            // console.log(text);
-            // const badge = document.createElement("p");
-            // // Use the same styling as the publish information in an article's header
-            // badge.classList.add("color-secondary-text", "type--caption");
-            // badge.textContent = `No articles found in result ¯\\_(ツ)_/¯`;
-            // result.insertAdjacentElement("afterend", badge);
-          }
-
-          const badge = document.createElement("div");
-          ReactDOM.render(<ThemeProvider theme={getTheme()}><ChipsArray /></ThemeProvider>, badge);
-          result.firstElementChild.insertAdjacentElement("afterbegin", badge);
+      let t = '';
+      chrome.runtime.sendMessage({type: "searchResult", url: url}, (text: string)  => {
+        t = text;
+        // const parser = new DOMParser();
+        // const doc = parser.parseFromString(text, 'text/html');
       });
+      console.log(url);
+      const badge = document.createElement("div");
+      ReactDOM.render(<ThemeProvider theme={getTheme()}><ChipsArray /></ThemeProvider>, badge);
+      result.parentElement.insertBefore(badge, result);
     }
 
     // Add or remove classes to matches results
